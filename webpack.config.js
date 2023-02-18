ea36e40f9970fac48e89b1b48a232fb230fa9f74
Cohen Erickson <cohenerickson@gmail.com>
2023-02-18 14:50:21 -0600

    init

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    unity: "./src/unity/index.ts"
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].js"
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public")
+    },
+    compress: true,
+    port: 3000
+  }
+};
